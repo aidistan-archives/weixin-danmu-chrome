e0f991caa1f09915706ad09e528419ff356f04e0
Aidi Stan <aidistan@live.com>
2015-09-14 12:40:31 +0800

    Forked from firefox-weixin-danmu

---
+++ data/danmu.js
@@ -0,0 +1,156 @@
+var prefs;
+
+var colors   = [
+  '#00aeef', // blue
+  '#ea428a', // red
+  '#eed500', // yellow
+  '#f5a70d', // orange
+  '#8bcb30', // green
+  '#9962c1', // purple
+  '#333333', // black
+  '#e8e8e8'  // white
+];
+
+var ctlptn = {
+  all: RegExp('^[:：]([蓝红黄橙绿紫黑白巨大小顶底])'),
+  color: RegExp('^[:：]([蓝红黄橙绿紫黑白])'),
+  size: RegExp('^[:：]([巨大小])'),
+  position: RegExp('^[:：]([顶底])')
+};
+
+var limit = {
+  font:  { min: 10, max: window.innerHeight/4 },
+  image: { width: window.innerWidth/3, height: window.innerHeight/3 }
+};
+
+$(window).resize(function() {
+  limit.font.max     = window.innerHeight / 4;
+  limit.image.width  = window.innerWidth / 3;
+  limit.image.height = window.innerHeight / 3;
+});
+
+self.port.on('setup', function(msg) { prefs = msg; });
+
+self.port.on('bullet', function(msg) {
+  // Defaults
+  var fontSize = prefs.defaultfontSize;
+  var color  = colors[Math.floor(Math.random() * colors.length)];
+  var top    = 'auto';
+  var bottom = 'auto';
+
+  while (ctlptn.all.test(msg.content.text)) {
+    // Color controls
+    if (ctlptn.color.test(msg.content.text)) {
+      switch (ctlptn.color.exec(msg.content.text)[1]) {
+        case '蓝':
+          color = '#00aeef';
+          break;
+        case '红':
+          color = '#ea428a';
+          break;
+        case '黄':
+          color = '#eed500';
+          break;
+        case '橙':
+          color = '#f5a70d';
+          break;
+        case '绿':
+          color = '#8bcb30';
+          break;
+        case '紫':
+          color = '#9962c1';
+          break;
+        case '黑':
+          color = '#333333';
+          break;
+        case '白':
+          color = '#eeeeee';
+          break;
+      }
+      msg.content.text = msg.content.text.replace(ctlptn.color, '');
+    }
+
+    // Size controls
+    if (ctlptn.size.test(msg.content.text)) {
+      switch (ctlptn.size.exec(msg.content.text)[1]) {
+        case '大':
+          fontSize *= 1.5;
+          fontSize = (fontSize > limit.font.max) ? limit.font.max : fontSize;
+          break;
+        case '小':
+          fontSize /= 1.5;
+          fontSize = (fontSize < limit.font.min) ? limit.font.min : fontSize;
+          break;
+        case '巨':
+          fontSize = limit.font.max;
+          break;
+      }
+      msg.content.text = msg.content.text.replace(ctlptn.size, '');
+    }
+
+    // Position controls
+    if (ctlptn.position.test(msg.content.text)) {
+      if (ctlptn.position.exec(msg.content.text)[1] == '顶') {
+        top = 12;
+      }
+      else {
+        bottom = 12;
+      }
+      msg.content.text = msg.content.text.replace(ctlptn.position, '');
+    }
+  }
+
+  // Make bullet
+  var bullet;
+  if (msg.content.image === '') {
+    bullet = $('<div>' + msg.content.text.replace(/<img/g, '<img height=' + fontSize) + '</div>');
+  }
+  else {
+    var img = $('<img src="' + msg.content.image + '" />').css({
+      'max-width': limit.image.width,
+      'max-height': limit.image.height
+    });
+    bullet = $('<div></div>').append(img);
+
+    // To fix image position issue
+    fontSize = limit.image.height;
+  }
+  if (prefs.showUsername) {
+    bullet.prepend($('<span>' + msg.user.name + ' : </span>'));
+  }
+  bullet.addClass('danmu-bullet').css({
+    color: color,
+    'font-size': fontSize,
+    'font-weight': 'bold',
+    'white-space': 'nowrap',
+    'overflow-y': 'visible'
+  });
+
+  // Insert first to get proper width and height
+  var fullElement = document.mozFullScreenElement;
+  bullet.hide().appendTo(fullElement ? $(fullElement) : $('body'));
+  if (top == 'auto' && bottom == 'auto') {
+    top = ~~(Math.random() * (window.innerHeight - bullet.height()));
+  }
+
+  // Place at the right place
+  bullet.css({
+    'z-index': 10000,
+    display: 'block',
+    position: 'fixed',
+    top: top,
+    bottom: bottom,
+    left: window.innerWidth,
+    width: bullet.width()
+  }).show();
+
+  // Fire the animation
+  bullet.animate({
+      left: -bullet.width()
+    }, ~~(Math.random() * 15) + 15000, 'linear', function() {
+      if (-$(this).css('left').slice(0, -2) - 100 <= bullet.width()) {
+        $(this).remove();
+      }
+    }
+  );
+});
---
+++ data/weixin.js
@@ -0,0 +1,110 @@
+var port = self.port;
+var src_pref, msg_read;
+
+// Whether is the target page
+if (document.getElementById('chatArea')) {
+  // Initialize
+  src_pref = window.location.protocol + '//' + window.location.host;
+  msg_read = new Array(40);
+
+  // Send heartbeats
+  setInterval(function() {
+    if ($('#chatArea').css('visibility') == 'visible') {
+      port.emit('heartbeat', true);
+    }
+  }, 3000);
+
+  // Listen to DOM mutations
+  new MutationObserver(function() {
+    sendMessage(captureMessage());
+  }).observe(document.getElementById('chatArea'), { 'childList': true, 'subtree': true });
+}
+
+/*
+  An typical message looks like:
+
+  {
+    id: '',
+    user: { name: '',  avatar: '' },
+    content: { text: '', image: '' }
+  };
+*/
+
+function captureMessage() {
+  var text = '', img = '';
+
+  // Get unique selector
+  var selector = $('#chatArea .message:last > .content > .ng-scope[data-cm]').attr('data-cm');
+  var id = $.parseJSON(selector).msgId;
+
+  // Check the message if read
+  for (i = 0; i < msg_read.length; i++) {
+    if (msg_read[i] == id) {
+      return null;
+    }
+  }
+
+  // Mark as read
+  msg_read.shift();
+  msg_read.push(id);
+
+  // HACK for issue #18
+  if (id.length < 18) {
+    return null;
+  }
+
+  // Handle text message
+  if ($('#chatArea .message:last .plain').length) {
+    if ($('#chatArea .message:last .plain pre img').length) {
+      $('#chatArea .message:last .plain pre img').each(function() {
+        if (/^\//.test($(this).attr('src'))) {
+          $(this).attr('src', src_pref + $(this).attr('src'));
+        }
+      });
+    }
+    if ($('#chatArea .message:last .plain pre img.emoji').length) {
+      $('#chatArea .message:last .plain pre img.emoji').each(function() {
+        $(this).replaceWith('<img height=24 src="http://cdn.bootcss.com/twemoji/1.2.0/svg/' + /emoji(\w+)/.exec(this.className)[1] + '.svg" />');
+      });
+    }
+    text = $('#chatArea .message:last .plain pre').html();
+
+  // Handle image message
+  } else if ($('#chatArea .message:last .picture .msg-img').length &&
+  /^\//.test($('#chatArea .message:last .picture .msg-img').attr('src'))) {
+    img = src_pref + $('#chatArea .message:last .picture .msg-img').attr('src').split('&type=slave')[0];
+
+  // Handle custom_emoji message
+  } else if ($('#chatArea .message:last .emoticon .custom_emoji').length) {
+    img = src_pref + $('#chatArea .message:last .custom_emoji').attr('src');
+
+  // Capture nothing
+  } else {
+    return null;
+  }
+
+  // Make message
+  var msg = {
+    id: id,
+    user: {
+      name: $('#chatArea .message:last .avatar').attr('title'),
+      avatar: src_pref + $('#chatArea .message:last .avatar').attr('src')
+    },
+    content: {
+      text: text,
+      image: img
+    },
+    selector: selector
+  };
+
+  return msg;
+}
+
+function sendMessage(msg) {
+  if (msg) {
+    setTimeout(function() {
+      msg.room = $("[data-cm='" + msg.selector + "']").length > 0 ? 'inside' : 'outside';
+      port.emit('bullet', msg);
+    }, 100);
+  }
+}
